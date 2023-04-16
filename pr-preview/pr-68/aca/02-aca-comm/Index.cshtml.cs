baf974d81348d3193d0e0b43c095eee76048fd41
pankajagrawal16 <pankajagrawal16@users.noreply.github.com>
2023-04-16 12:20:42 +0000

    Deploy preview for PR 68 🛫

---
+++ pr-preview/pr-68/aca/02-aca-comm/Index.cshtml.cs
@@ -0,0 +1,29 @@
+ namespace TasksTracker.WebPortal.Frontend.Ui.Pages
+    {
+        [IgnoreAntiforgeryToken(Order = 1001)]
+        public class IndexModel : PageModel
+        {
+            private readonly ILogger<IndexModel> _logger;
+            [BindProperty]
+            public string TasksCreatedBy { get; set; }
+
+            public IndexModel(ILogger<IndexModel> logger)
+            {
+                _logger = logger;
+            }
+
+            public void OnGet()
+            {
+            }
+
+            public IActionResult OnPost()
+            {
+                if (!string.IsNullOrEmpty(TasksCreatedBy))
+                {
+                    Response.Cookies.Append("TasksCreatedByCookie", TasksCreatedBy);
+                }
+
+                return RedirectToPage("./Tasks/Index");
+            }
+        }
+    }
