67265b8d45183cc1215014414b255bb3ed963c08
Simon Kurtz <simonkurtz@microsoft.com>
2023-10-10 07:36:20 -0400

    Clean-up

--- docs/aca/05-aca-dapr-pubsubapi/TasksNotifierController.cs
+++ docs/aca/05-aca-dapr-pubsubapi/TasksNotifierController.cs
@@ -13,6 +13,7 @@ namespace TasksTracker.Processor.Backend.Svc.Controllers
         private readonly IConfiguration _config;
         private readonly ILogger _logger;
         private readonly DaprClient _daprClient;
+        
         public TasksNotifierController(IConfiguration config, ILogger<TasksNotifierController> logger, DaprClient daprClient)
         {
             _config = config;
@@ -25,20 +26,19 @@ namespace TasksTracker.Processor.Backend.Svc.Controllers
         public async Task<IActionResult> TaskSaved([FromBody] TaskModel taskModel)
         {
             _logger.LogInformation("Started processing message with Task Name '{0}'", taskModel.TaskName);
- 
+
             var sendGridResponse = await SendEmail(taskModel);
- 
+
             if (sendGridResponse.Item1)
             {
-                return Ok($"SendGrid response staus code: {sendGridResponse.Item1}");
+                return Ok($"SendGrid response status code: {sendGridResponse.Item1}");
             }
- 
+
             return BadRequest($"Failed to send email, SendGrid response status code: {sendGridResponse.Item1}");
         }
- 
+
         private async Task<Tuple<bool, string>> SendEmail(TaskModel taskModel)
         {
- 
             var apiKey = _config.GetValue<string>("SendGrid:ApiKey");
             var sendEmailResponse = true;
             var sendEmailStatusCode = System.Net.HttpStatusCode.Accepted;
@@ -49,13 +49,12 @@ namespace TasksTracker.Processor.Backend.Svc.Controllers
             var plainTextContent = $"Task '{taskModel.TaskName}' is assigned to you. Task should be completed by the end of: {taskModel.TaskDueDate.ToString("dd/MM/yyyy")}";
             var htmlContent = plainTextContent;
             var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
-           
-	     var response = await client.SendEmailAsync(msg);
-	     sendEmailResponse = response.IsSuccessStatusCode;
-	     sendEmailStatusCode = response.StatusCode;
-           
-	     return new Tuple<bool, string>(sendEmailResponse, sendEmailStatusCode.ToString());
- 
+
+            var response = await client.SendEmailAsync(msg);
+            sendEmailResponse = response.IsSuccessStatusCode;
+            sendEmailStatusCode = response.StatusCode;
+
+            return new Tuple<bool, string>(sendEmailResponse, sendEmailStatusCode.ToString());
         }
     }
 }
