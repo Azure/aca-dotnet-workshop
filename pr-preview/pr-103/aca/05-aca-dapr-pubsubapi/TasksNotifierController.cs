b5d73330ae4835be5fa05b26277ff605b84965b2
simonkurtz-MSFT <simonkurtz-MSFT@users.noreply.github.com>
2023-10-10 11:57:44 +0000

    Deploy preview for PR 103 🛫

---
+++ pr-preview/pr-103/aca/05-aca-dapr-pubsubapi/TasksNotifierController.cs
@@ -0,0 +1,32 @@
+using Dapr.Client;
+using Microsoft.AspNetCore.Mvc;
+using TasksTracker.Processor.Backend.Svc.Models;
+
+namespace TasksTracker.Processor.Backend.Svc.Controllers
+{
+    [Route("api/tasksnotifier")]
+    [ApiController]
+    public class TasksNotifierController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        private readonly ILogger _logger;
+        private readonly DaprClient _daprClient;
+
+        public TasksNotifierController(IConfiguration config, ILogger<TasksNotifierController> logger, DaprClient daprClient)
+        {
+            _config = config;
+            _logger = logger;
+            _daprClient = daprClient;
+        }
+
+        [Dapr.Topic("dapr-pubsub-servicebus", "tasksavedtopic")]
+        [HttpPost("tasksaved")]
+        public Task<IActionResult> TaskSaved([FromBody] TaskModel taskModel)
+        {
+            var msg = string.Format("Started processing message with Task Name '{0}'", taskModel.TaskName);
+            _logger.LogInformation("Started processing message with Task Name '{0}'", taskModel.TaskName);
+
+            return OK(msg);
+        }
+    }
+}
