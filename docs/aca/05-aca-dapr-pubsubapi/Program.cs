3dd7170bfee3fda1b8b61d15b7a60e014a616cff
Simon Kurtz <simonkurtz@microsoft.com>
2023-11-09 09:34:37 -0500

    Add whitespace to match generated Program.cs

--- docs/aca/05-aca-dapr-pubsubapi/Program.cs
+++ docs/aca/05-aca-dapr-pubsubapi/Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 builder.Services.AddControllers().AddDapr();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
