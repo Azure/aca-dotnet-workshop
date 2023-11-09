85193e32405bc55502f1ee6a2a593bd3937b132b
Simon Kurtz <simonkurtz@microsoft.com>
2023-11-09 07:06:12 -0500

    Externalize files

--- docs/aca/05-aca-dapr-pubsubapi/Program.cs
+++ docs/aca/05-aca-dapr-pubsubapi/Program.cs
@@ -1,19 +1,28 @@
-namespace TasksTracker.Processor.Backend.Svc
-    {
-        public class Program
-        {
-            public static void Main(string[] args)
-            {
-                var builder = WebApplication.CreateBuilder(args);
-                // Add services to the container.
-                builder.Services.AddControllers().AddDapr();
-                var app = builder.Build();
-                app.UseHttpsRedirection();
-                app.UseAuthorization();
-                app.UseCloudEvents();
-                app.MapControllers();
-                app.MapSubscribeHandler();
-                app.Run();
-            }
-        }
-    }
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers().AddDapr();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseCloudEvents();
+
+app.MapControllers();
+
+app.MapSubscribeHandler();
+
+app.Run();
