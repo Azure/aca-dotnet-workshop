780b945554681d0c718c07acc5bc52911646d60b
simonkurtz-MSFT <84809797+simonkurtz-MSFT@users.noreply.github.com>
2023-11-09 19:58:45 +0000

    Deploying to gh-pages from @ Azure/aca-dotnet-workshop@121e9e575508d158501727149273560d868c4f47 🚀

--- aca/01-deploy-api-to-aca/Program.cs
+++ aca/01-deploy-api-to-aca/Program.cs
@@ -3,6 +3,7 @@ using TasksTracker.TasksManager.Backend.Api.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 builder.Services.AddSingleton<ITasksManager, FakeTasksManager>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
