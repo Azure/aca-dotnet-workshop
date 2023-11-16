995974532566d8f2b9a84d29d23cec86c88d291b
simonkurtz-MSFT <84809797+simonkurtz-MSFT@users.noreply.github.com>
2023-11-16 03:12:12 +0000

    Deploying to gh-pages from @ Azure/aca-dotnet-workshop@bfc749a66d2eaf917a3e1d9eeb7a3ad9c14921e4 🚀

--- aca/08-aca-monitoring/AppInsightsTelemetryInitializer-Frontend.UI.cs
+++ aca/08-aca-monitoring/AppInsightsTelemetryInitializer-Frontend.UI.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Channel;
 using Microsoft.ApplicationInsights.Extensibility;
 
-namespace TasksTracker.Processor.Backend.Svc
+namespace TasksTracker.TasksManager.Frontend.Ui
 {
     public class AppInsightsTelemetryInitializer : ITelemetryInitializer
     {
@@ -10,7 +10,7 @@ namespace TasksTracker.Processor.Backend.Svc
             if (string.IsNullOrEmpty(telemetry.Context.Cloud.RoleName))
             {
                 //set custom role name here
-                telemetry.Context.Cloud.RoleName = "tasksmanager-backend-processor";
+                telemetry.Context.Cloud.RoleName = "tasksmanager-frontend-webapp";
             }
         }
     }
