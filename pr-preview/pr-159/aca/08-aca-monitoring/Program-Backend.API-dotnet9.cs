94ec56a309fae4d0fc658136faae9f2302332c01
simonkurtz-MSFT <simonkurtz-MSFT@users.noreply.github.com>
2024-11-09 10:21:06 +0000

    Deploy preview for PR 159 🛫

---
+++ pr-preview/pr-159/aca/08-aca-monitoring/Program-Backend.API-dotnet9.cs
@@ -0,0 +1,32 @@
+using Microsoft.ApplicationInsights.Extensibility;
+using TasksTracker.TasksManager.Backend.Api.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddApplicationInsightsTelemetry();
+builder.Services.Configure<TelemetryConfiguration>((o) => {
+    o.TelemetryInitializers.Add(new AppInsightsTelemetryInitializer());
+});
+
+builder.Services.AddSingleton<ITasksManager, FakeTasksManager>();
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ pr-preview/pr-159/aca/08-aca-monitoring/Program-Backend.Svc-dotnet9.cs
@@ -0,0 +1,35 @@
+using Microsoft.ApplicationInsights.Extensibility;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddApplicationInsightsTelemetry();
+builder.Services.Configure<TelemetryConfiguration>((o) => {
+    o.TelemetryInitializers.Add(new AppInsightsTelemetryInitializer());
+});
+
+builder.Services.AddControllers().AddDapr();
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseCloudEvents();
+
+app.MapControllers();
+
+app.MapSubscribeHandler();
+
+app.Run();
