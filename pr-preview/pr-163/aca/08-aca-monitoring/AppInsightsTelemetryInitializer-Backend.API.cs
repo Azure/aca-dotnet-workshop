0fc80e8fc29029233556a94c93c232fda3665ef7
simonkurtz-MSFT <simonkurtz-MSFT@users.noreply.github.com>
2024-11-11 11:36:41 +0000

    Deploy preview for PR 163 🛫

---
+++ pr-preview/pr-163/aca/08-aca-monitoring/AppInsightsTelemetryInitializer-Backend.API.cs
@@ -0,0 +1,17 @@
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Extensibility;
+
+namespace TasksTracker.TasksManager.Backend.Api
+{
+    public class AppInsightsTelemetryInitializer : ITelemetryInitializer
+    {
+        public void Initialize(ITelemetry telemetry)
+        {
+            if (string.IsNullOrEmpty(telemetry.Context.Cloud.RoleName))
+            {
+                //set custom role name here
+                telemetry.Context.Cloud.RoleName = "tasksmanager-backend-api";
+            }
+        }
+    }
+}
